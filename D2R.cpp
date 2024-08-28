cfd3b93291b9081df7b9076d46df2543cdbd60eb
Prince Kumar <118840584+PrinceXkumar@users.noreply.github.com>
2024-08-28 22:34:16 +0530

    Add files via upload
    In this I studied about Data type, operators and string (Accessing, removing and modifying the string)

---
+++ D2R.cpp
@@ -0,0 +1,123 @@
+/* Write a program to add two numbers
+
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b;
+	cout<<"Enter the numbers : ";
+	cin>>a>>b;
+	int sum = a+b;
+	cout<<"Sum is : "<<sum<<endl;
+	return 0;
+	
+}
+
+*/
+
+//Write a program to find the remainder of a division
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int a ;
+	int b;
+	cin>>a>>b;
+	int rem = a%b;
+	cout<<rem;
+	return  0;
+}
+*/
+
+
+//Write a program to check if two numbers are equal.
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int a=10;
+//	int b=5;
+//	if(a>b || a!=b){
+//		cout<<"yes";
+//	}else{
+//		cout<<"No";
+//	}
+//}
+
+
+//							STRING
+
+//it is sequance of charectors 
+//and it is a object in c++
+
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string f_name = "Prince ";
+	string l_name= "Kumar";
+	cout<<f_name.append(l_name);
+}
+*/
+
+//Take string input from user
+/*
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	string name ;
+	cout<<"enter the name of the college : ";
+	getline(cin,name);  // for taking full sentence use getline 
+	cout<<"The name of the college is : "<<name <<endl;
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
